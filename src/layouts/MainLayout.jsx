3e859113f32a30db7ca312e1a9e55fd9af158a25
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-03-16 03:09:49 +0200

    fix MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer/footer'
+import Footer from '../components/footer/Footer'
 
 export default function MainLayout() {
   return (
